5b94502e485250c58c8a21b4f6032edc14ab408f
Federico Loro Meyer <fjloromeyer@gmail.com>
2023-01-29 02:14:26 +0100

    added major changes to main.js and ids to the meals in the htmls

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const VAT = 0.21; //Value added tax = 21%
 
 class Product{
-    constructor(name, price, category){
+    constructor(number, name, price, category){
+        this.number = number
         this.name = name;
         this.price = price;
         this.category = category;
@@ -13,25 +14,25 @@ class Product{
 };
 
 /* Starters */
-let meatEmpanada = new Product("meat empanada", 4, "starters");
-let provoleta = new Product("provoleta", 5, "starters");
-let chorizo = new Product("chorizo", 3, "starters");
+let meatEmpanada = new Product(1.1, "Meat empanada", 4, "starters");
+let provoleta = new Product(1.2, "Provoleta", 5, "starters");
+let chorizo = new Product(1.3, "Chorizo", 3, "starters");
 /* Main courses */
-let meatSchnitzel = new Product("meat schnitzel", 12, "main courses");
-let tbone = new Product("tbone", 17, "main courses");
-let asado = new Product("asado", 25, "main courses");
+let meatSchnitzel = new Product(2.1, "Meat schnitzel", 12, "main courses");
+let tbone = new Product(2.2, "T-bone steak", 17, "main courses");
+let asado = new Product(2.3, "Argentinian asado", 25, "main courses");
 /* Desserts */
-let panqueque = new Product("panqueque", 7, "desserts");
-let iceCream = new Product("ice cream", 5.5, "desserts");
-let strawberries = new Product("strawberries", 6, "desserts");
+let panqueque = new Product(3.1, "Panqueque with Dulce de Leche/Apple", 7, "desserts");
+let iceCream = new Product(3.2, "Ice cream", 5.5, "desserts");
+let strawberries = new Product(3.3, "Strawberries with cream", 6, "desserts");
 /* Drinks */
-let stillWater = new Product("still water", 3, "drinks");
-let sparklingWater = new Product("sparkling water", 3, "drinks");
-let soda = new Product("soda", 3.5, "drinks");
+let stillWater = new Product(4.1, "Still water", 3, "drinks");
+let sparklingWater = new Product(4.2, "Sparkling water", 3, "drinks");
+let soda = new Product(4.3, "Soda", 3.5, "drinks");
 /* Wines */
-let catenaZapata = new Product("catena zapata", 20, "wines");
-let elEnemigo = new Product("el enemigo", 30, "wines");
-let terrazas = new Product("terrazas reserva malbec", 18, "wines");
+let catenaZapata = new Product(5.1, "Catena Zapata", 20, "wines");
+let elEnemigo = new Product(5.2, "El Enemigo", 30, "wines");
+let terrazas = new Product(5.3, "Terrazas Reserva Malbec", 18, "wines");
 
 const menu = [meatEmpanada, provoleta, chorizo, meatSchnitzel, tbone, asado, panqueque, iceCream, strawberries, stillWater, sparklingWater, soda, catenaZapata, elEnemigo, terrazas]
 
@@ -46,7 +47,7 @@ while(access !== "client" && access !== "employee"){
 
 let selection
 if (access === "client"){
-    selection = prompt("Let's begin then! Have a look at the menu and choose what you want to order today. Separate them with commas.")
+    selection = prompt("Let's begin then! Have a look at the menu and tell me the ID of what you want to order today.")
 } else if (access === "employee"){
     console.log(access)
     alert("You should not be consulting this menu, you should already know our prices!")
@@ -54,7 +55,17 @@ if (access === "client"){
 
 console.log(selection)
 
-// let order = prompt("What dishes do you want to order today?")
+let sumPrices = 0
+for (i=0; i<menu.length; i++) {
+    if (menu[i].number == selection) {
+        sumPrices = sumPrices + menu[i].price
+        alert("Ordering this will cost you " + sumPrices + ".")
+    }
+}
+
+if (sumPrices === 0) {
+    alert("You didn't choose a correct dish.")
+}
 
 
 /* console.log("The price of meat empanada is " + meatEmpanada.price + " and adding the VAT the total would be " + meatEmpanada.sumVAT()); */
