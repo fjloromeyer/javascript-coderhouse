79206e522c4b296e9c724ab3d8da5c81a60dabe1
Federico Loro Meyer <fjloromeyer@gmail.com>
2023-01-30 23:19:31 +0100

    1st deadline finished

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
     
-function runHtmlBeforeJavascript() {
+function runHtmlBeforeJavascript() { //this function was made to have the html run before javascript code, so client can look at the menu before answering prompts.
     const VAT = 0.21; //Value added tax = 21%
 
     class Product{
@@ -14,6 +14,11 @@ function runHtmlBeforeJavascript() {
             return this.price * (1+VAT);
         }
     };
+    
+    //create function to know if something is a float or not.
+    function isFloat(n){
+        return Number(n) === n && n % 1 !== 0;
+    }
 
     /* Starters */
     let meatEmpanada = new Product(1.1, "Meat empanada", 4, "starters");
@@ -53,48 +58,65 @@ function runHtmlBeforeJavascript() {
     if (access === "client"){
         selection = prompt("Let's begin then! Have a look at the menu and tell me the ID of what you want to order today.")
     } else if (access === "employee"){
-        console.log(access)
         alert("You should not be consulting this menu, you should already know our prices!")
     }
     
     // going through the menu list and if the ID entered by the client matches the ID of one of our objects in the menu, get the price and sum it in sumPrices
-    for (i=0; i<menu.length; i++) {
-        if (menu[i].number == selection) {
-            sumPrices = sumPrices + menu[i].price
-            alert("Ordering this will cost you " + sumPrices + " USD.") //later on adding feature to put as many dishes as one wants and summing all to have the check
+    function cost (foodId) {
+        for (i=0; i<menu.length; i++) {
+            if (menu[i].number == foodId) {
+                sumPrices = sumPrices + menu[i].price
+                alert("Ordering this will cost you " + sumPrices + " USD.") //later on adding feature to put as many dishes as one wants and summing all to have the check
+            }
         }
     }
 
-    // if the user is an employee do nothing. If it is a client and selected a wrong ID, asking to select again.
+    cost(selection)
+
+    // if the user is an employee do nothing. If it is a client and selected a wrong ID, ask to select again.
     if (sumPrices === 0 && access === "employee") {
         pass
     } else if (sumPrices === 0) {
-        alert("You didn't choose a correct dish. Please enter only the ID at the left of the name of the dish that you want to order, for example 1.2")
+        let selectionNew = 0
+        const menuIds = [1.1, 1.2, 1.3, 2.1, 2.2, 2.3, 3.1, 3.2, 3.3, 4.1, 4.2, 4.3, 5.1, 5.2, 5.3]
+        while (isFloat(selectionNew) === false || menuIds.includes(selectionNew) === false) {
+            selectionNew = parseFloat(prompt("You didn't choose a correct dish. Please enter only the ID at the left of the name of the dish that you want to order, for example 1.2"))
+        }
+        cost(selectionNew)
     }
 
+    // suggest a tip and take into account if the user types a float with comma or dot.
     let suggestedTip = 0.1*sumPrices;
-    let tip = parseFloat(prompt("Do you want to add a tip? (we suggest 10%, that would be " + suggestedTip + " USD. Please enter only the number."));
-    console.log(tip)
+    let tip
+    if (access === "employee") {
+        pass
+    } else {
+        tip = prompt("Do you want to add a tip? (we suggest 10%, that would be " + suggestedTip + " USD. Please enter only the number separated with a dot, example: 1.2.");
+        if (tip.includes(",")){
+            tip = tip.replace(",",".")
+        }
+        tip = parseFloat(tip)
+    }
 
-/*     while(access !== "client" && access !== "employee"){
-        alert("Intruder!!!")
-        access = prompt("Let's try again... are you a client or an employee?").toLowerCase() //putting the prompt in lower case.
-    } */
+    // if a string is typed in the previous prompt, no tip would be the default.
+    if (tip/1 != tip) {
+        alert("In that case, I understand that no tip would be included.")
+    }
 
-    function calcTip(){
-        let finalTip = 0;
-        if (tip >= 0) {
-            finalTip == tip;
-            }
-        else {
-            finalTip == 0
+    // do not take into account tips that are negative, no tip would be the default.
+    function calcTip(money){
+        let finalTip;
+        if (money >= 0) {
+            finalTip = money;
+        } else {
+            finalTip = 0
         };
         return finalTip
     }
 
-    let totalNet = sumPrices + calcTip()
-
-    console.log("The total of the meal plus tip would be " + totalNet + " USD.")
+    //calculate and inform the total net of the whole meal.
+    let totalNet = sumPrices + calcTip(tip)
+    alert("The total of the meal plus tip would be " + totalNet + " USD.")
 }
 
 const elemento = document.getElementById("beginOrder")
